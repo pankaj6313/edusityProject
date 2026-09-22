1c48158df59ee3a615df1c6c811e52d9e99b09ad
pankaj6313 <rthe2036@gmail.com>
2026-09-22 10:32:22 +0530

    Rename Program js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Program from './components/Program/Programrogram'
+
 
 function App() {
   return (
     <div>
      <Navbar/>
      <Hero/>
+     <Program/>
     </div>
   )
 }
