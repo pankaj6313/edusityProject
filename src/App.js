324bd30eaad020d28d0014c4dc01006b8f9a5355
pankaj6313 <rthe2036@gmail.com>
2026-09-22 22:28:41 +0530

    Add Testimonial component

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import Program from './components/Programs/Program'
 import Title from './components/Title/Title'
 import About from './components/About/About'
 import Campus from './components/Campus/Campus'
+import Testimonials from './components/Testimonials/Testimonials'
 
 
 function App() {
   return (
     <div>
-     <Navbar/>
-     <Hero/>
-     <div className="container">
-      <Title subTitle='Our Program' title='What We Offer '/>
-      <Program/>
-      <About/>
-      <Title subTitle='Gallery' title='Campus Photos'/>
-      <Campus/>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle='Our Program' title='What We Offer ' />
+        <Program />
+        <About />
+        <Title subTitle='Gallery' title='Campus Photos' />
+        <Campus />
+        <Title subTitle='TESTIMONIALS' title='What Student Says' />
+        <Testimonials />
       </div>
-     
+
     </div>
   )
 }
--- src/components/Campus/Campus.js
+++ src/components/Campus/Campus.js
@@ -9,15 +9,15 @@ import white_arrow from '../../assets/white-arrow.png'
 const Campus = () => {
   return (
     <div className='campus'>
-        <div className="gallery">
-            <img src={gallery_1} alt="" />
-            <img src={gallery_2} alt="" />
-            <img src={gallery_3} alt="" />
-            <img src={gallery_4} alt="" />
-        </div>
+      <div className="gallery">
+        <img src={gallery_1} alt="" />
+        <img src={gallery_2} alt="" />
+        <img src={gallery_3} alt="" />
+        <img src={gallery_4} alt="" />
+      </div>
 
-        <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
-      
+      <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
+
     </div>
   )
 }
