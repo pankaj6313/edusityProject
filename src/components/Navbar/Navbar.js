370979ede753d4a70ff6731dd0d3e92d8336b324
pankaj6313 <rthe2036@gmail.com>
2026-09-22 09:42:38 +0530

    Add Initial file

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='container'>
+        <img src={logo} alt="" />
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+            <li><button className='btn'>Contact us</button></li>
+        </ul>
+        
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
