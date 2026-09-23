6d9dc1f98121c65f99381ebafd78973b5b3c8b6a
pankaj6313 <rthe2036@gmail.com>
2026-09-23 19:12:41 +0530

    Add Contact component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Title from './components/Title/Title'
 import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
 
 
 function App() {
@@ -21,6 +22,8 @@ function App() {
         <Campus />
         <Title subTitle='TESTIMONIALS' title='What Student Says' />
         <Testimonials />
+        <Title subTitle='Contact Us' title='Get in Touch'/>
+        <Contact/>
       </div>
 
     </div>
